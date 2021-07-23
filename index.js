0f6cd00bf70935a23c8b6fc9cfa2dd9c996924f1
kejsi <kejsi1994@yahoo.com>
2021-07-22 22:55:22 -0400

    commit tree 8a4adab38afdd5f7ff02b96a2461f90b5080a9d7

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-// Code your solutions in this file
-const gifts = ["teddy bear", "drone", "doll"];
-
-function wrapGifts(gifts) {
-  for (let i = 0; i < gifts.length; i++) {
-    console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
-  }
-
-  return gifts;
-}
-
-wrapGifts(gifts);
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+  debugger;
+} 
